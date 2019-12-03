15aef6c8a1b1d06feaaf7ef43a8221c10472efb1
digitalmoleman <digitalmoleguy@gmail.com>
2019-12-03 11:09:32 +0100

    request functionality, response model

--- index.js
+++ index.js
@@ -14,22 +14,23 @@ firebase.initializeApp(config.firebase);
 
 const db = firebase.firestore();
 
-var serverCache = {
-    users: []
-}
 
-getServerCache = () => {
-    console.log(userCache);
-}
-
-updateCachedUsers = () => {
-    let userCache = []
-    db.collection("users").get().then(querySnapshot => {
-        querySnapshot.forEach(doc => userCache.push(doc.data()));
+/**
+ * 
+ * @param {String} collection 
+ */
+function getCollection(collection) {
+    return new Promise(resolve => {
+        db.collection(collection).get().then(querySnapshot => {
+            let result = [];
+            querySnapshot.forEach(doc => {
+                let document = doc.data();
+                document['id'] = doc.id;
+                result.push(document);
+            });
+            resolve(result)
+        });
     });
-    serverCache.users = userCache;
-
-    console.log(serverCache.users);
 }
 
 /**
@@ -37,122 +38,147 @@ updateCachedUsers = () => {
  * @param {String} field
  * @param {*} value
  */
-function getDocByFieldValue(collection, field, value) {
-    return new Promise((resolve, reject) => {
+function getDocsByFieldValue(collection, field, value) {
+    return new Promise(resolve => {
         db.collection(collection).where(field, "==", value).get().then(querySnapshot => {
-            querySnapshot.forEach(doc => resolve(doc.data()));
-            resolve(null);
+            let result = [];
+            querySnapshot.forEach(doc => {
+                let document = doc.data();
+                document['id'] = doc.id;
+                result.push(document);
+            });
+            resolve(result)
         });
     });
 }
 
 getUserByAppId = async (appId) => {
-    var user = await getDocByFieldValue("users", "app_id", appId);
-    if (user === null) console.log("ERROR", "could not get user");
-    return user;
+    let users = await getDocsByFieldValue("users", "app_id", appId);
+    return users[0];
+}
+
+getLatestRatingByUserId = async (userId) => {
+    let userRatings = await getDocsByFieldValue("ratings", "author_id", userId);
+    let latestRating = userRatings.sort((a, b) => (a.timestamp.seconds < b.timestamp.seconds) ? 1 : -1)[0];
+    return latestRating;
+}
+
+getFoodMenuByWeek = async (week) => {
+    let foodMenu = await getDocsByFieldValue("foodMenu", "week", week);
+    return foodMenu;
 }
 
 
-function getCollection(collection) {
-    return new Promise((resolve, reject) => {
-        db.collection(collection).get().then(querySnapshot => {
-            let docs = [];
-            querySnapshot.forEach(doc => docs.push(doc.data()));
-            resolve(docs)
-        });
-    });
-}
+app.post('/rate', async (req, res) => {
+    console.log(`[rate]`, req.query);
 
-class RateEvent {
-    constructor(user, rating) {
-        this.author_id = user.app_id;
-        this.rating = rating;
-        return this;
-    }
-}
+    let user = await getUserByAppId(req.query.app_id)
+    let rateEvent = {
+        author_id: user.id,
+        timestamp: new Date(),
+        rating: req.query.rating,
+    };
 
-app.post('/post_test', (req, res) => {
-    console.log(`[post_test]`);
-    updateCachedUsers();
-    res.send('post_test_response');
-})
-
-app.get('/rate_event', async (req, res) => {
-    console.log(`[rate_event]`);
-
-    let rateEvent = new RateEvent(
-        await getUserByAppId(req.query.app_id),
-        req.query.rating
-    );
-
-    console.log(rateEvent);
-
-    //db.collection("ratings").add(rateEvent);
-    res.send(rateEvent);
-});
-
-app.get('/rate_event_new', async (req, res) => {
-    console.log(`[rate_event]`);
-
-    let rateEvent = new RateEvent(
-        await getUserByAppId(req.query.app_id),
-        req.query.rating
-    );
+    if (req.comment) rateEvent.comment = req.comment
 
     console.log(rateEvent);
 
     db.collection("ratings").add(rateEvent);
-    res.send(rateEvent);
+
+    let response = new SuccessResponse("Rating Successful");
+    res.send(response);
 });
 
-app.get('/fetch_food_menu_event', async (req, res) => {
-    console.log(`[fetch_food_menu_event]`);
+app.post('/comment', async (req, res) => {
+    console.log(`/comment`, req.query);
+    let user = await getUserByAppId(req.query.app_id)
+    let latestRating = await getLatestRatingByUserId(user.id)
 
-    let foodMenu = await getCollection("foodMenu");
+    let updatedRating = {
+        author_id: latestRating.author_id,
+        rating: latestRating.rating,
+        timestamp: latestRating.timestamp,
+        comment: req.query.comment
+    }
+
+    console.log("Updated Rating", updatedRating);
+
+    db.collection("ratings").doc(latestRating.id).set(updatedRating);
+
+    let response = new SuccessResponse(res, "")
+    res.send(response);
+});
+
+app.get('/fetch_menu', async (req, res) => {
+    console.log(`[fetch_menu]`, req.query);
+    let foodMenu = await getFoodMenuByWeek(JSON.parse(req.query.week));
 
     console.log(foodMenu);
 
     res.send(foodMenu);
 });
 
-
-app.post('/comment_event', (req, res) => {
-    console.log(`[comment_event]`);
+app.post('/user_register', (req, res) => {
+    console.log(`[user_register]`, req.query);
     res.send('nice');
 });
 
-app.post('/user_register_event', (req, res) => {
-    console.log(`[user_register_event]`);
-    res.send('nice');
-});
-
-app.post('/generate_key_event', (req, res) => {
-    console.log(`[generate_key_event]`);
+app.post('/generate_key', (req, res) => {
+    console.log(`[generate_key]`, req.query);
     res.send('nice');
 });
 
 let adminToken = "test";
 
 app.get('/admin_fetch_data', async (req, res) => {
-    console.log(`[admin_fetch_data]`);
-    let response
+    console.log(`[admin_fetch_data]`, req.query);
+    let data
     if (req.query.admin_key = adminToken) {
 
         let users = await getCollection("users");
         let ratings = await getCollection("ratings");
         let comments = await getCollection("comments");
         let foodMenu = await getCollection("foodMenu");
-        response = {
+        data = {
             users: users,
             ratings: ratings,
             comments: comments,
             foodMenu: foodMenu
         }
 
-        console.log(response);
+        new SuccessResponse(res, "Data fetch successful", data);
     } else {
-        response = "invalid admin key"
+        new ErrorResponse(res, "Invalid admin key");
     }
 
-    res.send(response);
+    //res.send(response);
 })
+
+sendSuccessResponse = () => {
+
+
+
+    res.send(successResponse);
+}
+
+class Response {
+    constructor(res, message, status, params) {
+        this.message = message;
+        this.status = status;
+        for (let property in params) this[property] = params[property];
+        res.send(this);
+    }
+}
+
+class SuccessResponse extends Response {
+    constructor(res, message, params) {
+        super(res, message, "success", params);
+    }
+}
+
+
+class ErrorResponse extends Response {
+    constructor(res, message, params) {
+        super(res, message, "error", params);
+    }
+}
